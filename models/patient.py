670d8ee5b264b6b213279252e12f8432c2f3df15
FrankE <frankern98@gmail.com>
2023-03-24 13:36:57 -0400

    Primer commit consiente

---
+++ models/patient.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+class HospitalPatient(models.Model):
+    _name = "hospital.patient"
+    _description = "Hospital Patient"
+
+    name = fields.Char(string='Name')
+    age = fields.Integer(int="Age")
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], strin="gender")
+    bloodType = fields.Char(string='Blood Type')
